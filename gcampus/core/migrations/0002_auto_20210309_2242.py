06199ad835614e024faeecc30af230be16a704e3
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-03-10 03:26:44 +0100

    Migration for location_name

--- gcampus/core/migrations/0002_auto_20210309_2242.py
+++ gcampus/core/migrations/0002_auto_20210309_2242.py
@@ -7,30 +7,34 @@ import django.utils.timezone
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('gcampuscore', '0001_initial'),
+        ("gcampuscore", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='datapoint',
-            name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            model_name="datapoint",
+            name="created_at",
+            field=models.DateTimeField(
+                auto_now_add=True, default=django.utils.timezone.now
+            ),
             preserve_default=False,
         ),
         migrations.AddField(
-            model_name='datapoint',
-            name='updated_at',
+            model_name="datapoint",
+            name="updated_at",
             field=models.DateTimeField(auto_now=True),
         ),
         migrations.AddField(
-            model_name='measurement',
-            name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            model_name="measurement",
+            name="created_at",
+            field=models.DateTimeField(
+                auto_now_add=True, default=django.utils.timezone.now
+            ),
             preserve_default=False,
         ),
         migrations.AddField(
-            model_name='measurement',
-            name='updated_at',
+            model_name="measurement",
+            name="updated_at",
             field=models.DateTimeField(auto_now=True),
         ),
     ]
