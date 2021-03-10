07901afa454940f8ba007231035ec18b57d5b2cc
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-03-10 12:21:40 +0100

    Remove old GeoDjango admin, use leaflet

--- gcampus/core/admin.py
+++ gcampus/core/admin.py
@@ -1,15 +1,12 @@
 from django.contrib.gis import admin
+from leaflet.admin import LeafletGeoAdmin
 
 from gcampus.core.models import Measurement, DataType, DataPoint
 from gcampus.core.models.util import ADMIN_READ_ONLY_FIELDS
 
 
-class MeasurementAdmin(admin.OSMGeoAdmin):
-    default_lon = 1022432.1417261
-    default_lat = 6443998.3973475
+class MeasurementAdmin(LeafletGeoAdmin):
     readonly_fields = ADMIN_READ_ONLY_FIELDS + ("location_name",)
-    map_template = "gcampuscore/admin/osm.html"
-
 
 class DataTypeAdmin(admin.ModelAdmin):
     pass
