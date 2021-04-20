72c8a27b7c6eefaec47e534f3329af258742465b
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-04-20 17:29:08 +0200

    Add overpass API

--- gcampus/api/views/functions.py
+++ gcampus/api/views/functions.py
@@ -1,18 +1,89 @@
 from numbers import Number
 from typing import Tuple, Union
 
+import coreschema
+import overpy
+from django.http import HttpRequest
+from django.utils.decorators import method_decorator
+from django.utils.translation import gettext_lazy
+from django.views.decorators.cache import cache_page
+from overpy.exception import OverpassTooManyRequests
+from rest_framework.decorators import action
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import Distance
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers, status
+from rest_framework.exceptions import ParseError, Throttled
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from gcampus.api.serializers import GeoLookupSerializer
 
 
 class GeoLookupViewSet(viewsets.ViewSet):
-    pass
+    api = overpy.Overpass()
+    serializer = GeoLookupSerializer
+
+    @method_decorator(cache_page(60 * 60 * 24 * 2))  # Cache for two days
+    def list(self, request: Request, **kwargs):
+        params = request.query_params
+        coordinates = params.get("coords", None)
+        size = params.get("size", None)
+        srid = params.get("srid", 4326)
+        if coordinates is None:
+            raise serializers.ValidationError("'coords' parameter is required")
+        if size is None:
+            raise serializers.ValidationError("'size' parameter is required")
+        try:
+            x, y = str(coordinates).split(",")
+            x = float(x)
+            y = float(y)
+        except (TypeError, ValueError, OverflowError):
+            raise ParseError("Unable to parse 'coords'")
+        try:
+            size = float(size)
+        except (TypeError, ValueError, OverflowError):
+            raise ParseError("Unable to parse 'size'")
+        center = Point(x, y, srid=srid)
+        query = self._get_overpass_query(center, size)
+        try:
+            result = self.api.query(query)
+        except OverpassTooManyRequests:
+            raise Throttled
+        serializer = self.serializer()
+        return Response(
+            serializer.to_representation(result.get_ways()), status=status.HTTP_200_OK
+        )
+
+    def _get_overpass_query(self, center: Point, size: Union[Number, Distance]) -> str:
+        """Get Overpass Query
+
+        Generate the query for used for overpass
+
+        :param center: The center point
+        :param size: The square edge size. If size is a number (e.g. ``int``
+            or ``float``), the unit will be meters. The parameter can also
+            be of type Distance to easily convert between different units.
+        :return: String used to query overpass
+        :rtype: str
+        """
+        upper_left, lower_right = get_bbox_coordinates(center, size)
+        bbox = f"{upper_left.x},{upper_left.y},{lower_right.x},{lower_right.y}"
+        query = f"""
+        (
+            way["natural"="water"]({bbox});
+            way["waterway"]({bbox});
+            way["water"]({bbox});
+        );
+        out body;
+        >;
+        out skel qt;
+        """
+        return query
 
 
 def get_bbox_coordinates(
-    center: Point, size: Union[Number, Distance]
-) -> Tuple[Point, ...]:
+        center: Point, size: Union[Number, Distance]
+) -> Tuple[Point, Point]:
     """Get Bounding Box Coordinates
 
     Get the coordinates (two points at the corners) describing a square
@@ -29,7 +100,8 @@ def get_bbox_coordinates(
     :param size: The square edge size. If size is a number (e.g. ``int``
         or ``float``), the unit will be meters. The parameter can also
         be of type Distance to easily convert between different units.
-    :returns: Tuple of two points describing the square bounding box
+    :return: Tuple of two points describing the square bounding box
+    :rtype: Tuple[Point, Point]
     """
     if isinstance(size, Distance):
         size = size["m"]
@@ -40,7 +112,7 @@ def get_bbox_coordinates(
         )
     if center.srid is None:
         center.srid = 4326  # default coordinate system
-    if center.srid is not 3857:
+    if center.srid != 3857:
         center_meter: Point = center.clone()
         # Transform longitude and latitude in meters
         center_meter.transform(3857)
--- gcampus/api/views/models.py
+++ gcampus/api/views/models.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework_gis.pagination import GeoJsonPagination
 
 from gcampus.api.serializers import (
@@ -10,17 +11,18 @@ from gcampus.core.models import Measurement, DataType, DataPoint
 
 
 class MeasurementAPIViewSet(viewsets.ViewSetMixin, generics.ListAPIView):
-    queryset = Measurement.objects.all()
+    queryset = Measurement.objects.order_by('time').all()
     serializer_class = MeasurementSerializer
     pagination_class = GeoJsonPagination
 
 
 class DataTypeAPIViewSet(viewsets.ViewSetMixin, generics.ListAPIView):
-    queryset = DataType.objects.all()
+    queryset = DataType.objects.order_by('name').all()
     serializer_class = DataTypeSerializer
+    pagination_class = PageNumberPagination
 
 
 class DataPointAPIViewSet(viewsets.ViewSetMixin, generics.ListAPIView):
-    queryset = DataPoint.objects.all()
+    queryset = DataPoint.objects.order_by('measurement__time').all()
     serializer_class = DataPointSerializer
-    pagination_class = GeoJsonPagination
+    pagination_class = PageNumberPagination
