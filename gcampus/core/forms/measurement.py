877fe34204b6c3857e972a5deeeeff44bdde1912
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-05-12 12:08:44 +0200

    Hide delete button by default

--- gcampus/core/forms/measurement.py
+++ gcampus/core/forms/measurement.py
@@ -1,5 +1,6 @@
-from django.forms import ModelForm, modelformset_factory, inlineformset_factory
-from django.forms.widgets import Select, TextInput, Textarea
+from django.forms import ModelForm, modelformset_factory, inlineformset_factory, \
+    BaseInlineFormSet
+from django.forms.widgets import Select, TextInput, Textarea, HiddenInput
 from leaflet.forms.widgets import LeafletWidget
 from django.utils.translation import gettext_lazy as _
 
@@ -46,6 +47,22 @@ class DataPointForm(ModelForm):
         }
 
 
+class DynamicInlineFormset(BaseInlineFormSet):
+    _TEMPLATE: str = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for form in self.forms:
+            form.fields['DELETE'].widget = HiddenInput()
+
+    @property
+    def empty_form(self):
+        empty_form = super().empty_form
+        empty_form.fields['DELETE'].widget = HiddenInput()
+        return empty_form
+
+
 DataPointFormSet = inlineformset_factory(
-    Measurement, DataPoint, form=DataPointForm, can_delete=True
+    Measurement, DataPoint, form=DataPointForm, can_delete=True,
+    extra=0, formset=DynamicInlineFormset
 )
