88f397c97e30fa2bf24a58cdc3499478509b5adb
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-05-17 15:23:21 +0200

    Add permanent highlighting and a lot of comments

--- gcampus/core/static_src/js/watersuggestion.js
+++ gcampus/core/static_src/js/watersuggestion.js
@@ -14,11 +14,16 @@ const VAR_LOCATION_TITLE = window._varLocationTitle;
  * This method sends a request to the gcampus API to find nearby points
  * of interest with the tag "natural=water".
  *
- * @param lat Number or String - Latitude
- * @param lng Number or String - Longitude
+ * @param lat {number|string} - Latitude
+ * @param lng {number|string} - Longitude
  */
 function fetchWaterSuggestion(lat, lng) {
     let url = '/api/v1/geolookup/';
+    // Parse string inputs for rounding later
+    if (typeof lat !== 'number')
+        lat = Number.parseFloat(lat);
+    if (typeof lng !== 'number')
+        lng = Number.parseFloat(lng);
     // Round the location to 3 digits. This should be precise enough
     // A large bounding box is used.
     // This improves cache performance
@@ -34,6 +39,15 @@ function fetchWaterSuggestion(lat, lng) {
 }
 
 
+/**
+ * Water List Item
+ *
+ * Returns an element used to display a list of water suggestions inside
+ * the ``WaterList`` class.
+ *
+ * @param props
+ * @returns {VNode}
+ */
 function ListItem(props) {
     let {feature, highlight, resetHighlight} = props;
     let name = feature.properties.name;
@@ -62,6 +76,7 @@ class VariableWaterItem extends Component {
     setChecked(checked) {
         let el = document.getElementById('varWater');
         el.checked = checked;
+        el.dispatchEvent(new Event("change"));
     }
 
     createNameChangeListener() {
@@ -105,6 +120,7 @@ class WaterList extends Component {
     constructor() {
         super();
         this.state = {features: [], loading: false};
+        this.currentPermanentHighlight = null;
         window.addEventListener('map:init', this.mapInit.bind(this));
     }
 
@@ -122,10 +138,27 @@ class WaterList extends Component {
         this.setState({features: features});
     }
 
+    /**
+     * Set Loading
+     *
+     * Set the loading state. Setting this to true will trigger display
+     * the loading indicator.
+     * @param loading
+     */
     setLoading(loading) {
         this.setState({loading: loading});
     }
 
+    /**
+     * Update Style
+     *
+     * Update the styling (fill and edge color) of a feature layer.
+     * Used to highlight and reset features once they are hovered or
+     * selected.
+     *
+     * @param layer: Map layer of the feature
+     * @param color {String}: Fill and edge color
+     */
     updateStyle(layer, color) {
         let options = {
             fill: layer.options.fill,
@@ -136,7 +169,40 @@ class WaterList extends Component {
         layer.setStyle(options);
     }
 
+    /**
+     * Valid Feature ID
+     *
+     * Returns a valid feature ID (i.e. an integer instead of a string)
+     * and throws an exception if an invalid string or other type has
+     * been provided.
+     * This is needed because the feature layers use integer IDs whereas
+     * the value of HTMLElements are strings.
+     *
+     * @param featureID {number|string}: Feature ID
+     * @returns {number}
+     */
+    validFeatureID(featureID) {
+        if (typeof featureID !== 'number') {
+            try {
+                return Number.parseInt(featureID);
+            } catch {
+                throw Error(`Unable to parse feature ID '${featureID}'. Expected an integer!`);
+            }
+        }
+        return featureID;
+    }
+
+    /**
+     * Get Feature Layer
+     *
+     * Return the feature layer for a given feature ID. If no layer with
+     * matching ID can be found, an error is thrown.
+     *
+     * @param featureID {number|string}: Feature ID
+     * @returns: A map layer
+     */
     getFeatureLayer(featureID) {
+        featureID = this.validFeatureID(featureID);
         let featureLayer = this.layer.getLayers().filter(
             l => l.feature.id === featureID
         );
@@ -146,16 +212,60 @@ class WaterList extends Component {
         return featureLayer[0];
     }
 
+    /**
+     * Highlight Feature
+     *
+     * Highlight a feature layer by a given ID. This will change the
+     * color of the layer to visually distinguish one layer from the
+     * others.
+     *
+     * @param featureID {number|string}: Feature ID
+     */
     highlightFeature(featureID) {
         let featureLayer = this.getFeatureLayer(featureID);
         this.updateStyle(featureLayer, HIGHLIGHT_COLOR);
     }
 
+    /**
+     * Reset Highlight Feature
+     *
+     * Resets the highlighting of a given feature. This will always
+     * remove the highlighting, whether or not the feature is
+     * permanently highlighted (e.g. by being selected).
+     *
+     * @param featureID {number|string}: Feature ID
+     */
     resetHighlightFeature(featureID) {
         let featureLayer = this.getFeatureLayer(featureID);
         this.updateStyle(featureLayer, DEFAULT_COLOR);
     }
 
+    /**
+     *  Maybe Reset Highlight Feature
+     *
+     *  Only resets the highlighting of a given feature if the feature
+     *  is not currently permanently highlighted. Used for when the
+     *  element is no longer hovered.
+     *
+     * @param featureID {number|string}: Feature ID
+     */
+    maybeResetHighlightFeature(featureID) {
+        if (this.validFeatureID(featureID) === this.validFeatureID(this.currentPermanentHighlight)) {
+            // Do nothing
+            return;
+        }
+        this.resetHighlightFeature(featureID);
+    }
+
+    /**
+     * Update Map
+     *
+     * Called when the map is being edited (i.e. a marker is placed).
+     * Makes a request to the gcampus API which then requests the
+     * overpass API to find all waters in proximity to the marker.
+     *
+     * @param e {Event}: Event, passed by the event listener
+     */
     mapUpdate(e) {
         this.setFeatures([]);
         let marker = e.layer;
@@ -171,22 +281,98 @@ class WaterList extends Component {
         console.error(err);
     }
 
+    /**
+     * Initialize Map
+     *
+     * Saves the map and GeoJSON layer and registers the event listner
+     * for updating markers.
+     *
+     * @param e {Event}: Passed by the event listener for map
+     *      initialisation.
+     */
     mapInit(e) {
         this.map = e.detail.map;
         this.layer = L.geoJSON().addTo(this.map);
         this.map.on('draw:created draw:edited', this.mapUpdate.bind(this));
     }
 
+    /**
+     * Create mouseover Listener
+     *
+     * Returns a callable function that is used for registering event
+     * listeners in the ``render`` method.
+     *
+     * @param feature {Object}: Feature object. Only the id attribute
+     *      will be used.
+     * @returns {(function(Event): void)}
+     */
     createMouseOverListener(feature) {
         return (e) => {
             this.highlightFeature.bind(this)(feature.id);
         };
     }
 
+    /**
+     * Create mouseleave Listener
+     *
+     * Returns a callable function that is used for registering event
+     * listeners in the ``render`` method.
+     *
+     * @param feature {Object}: Feature object. Only the id attribute
+     *      will be used.
+     * @returns {(function(Event): void)}
+     */
     createMouseLeaveListener(feature) {
         return (e) => {
-            this.resetHighlightFeature.bind(this)(feature.id);
+            this.maybeResetHighlightFeature.bind(this)(feature.id);
         };
+    }
+
+    /**
+     * Register Change Listener of Radio Buttons
+     *
+     * Iterates over all radio buttons and adds an event listener to
+     * each. This will check if the radio button has been selected and
+     * permanently highlights the features associated with those radio
+     * buttons.
+     */
+    registerChangeListener() {
+        if (document.querySelector('input[name="location_name"]')) {
+            document.querySelectorAll('input[name="location_name"]').forEach(el => {
+                el.addEventListener('change', (e) => {
+                    if (e.target.checked) {
+                        // The element has actually been checked
+                        if (this.currentPermanentHighlight) {
+                            // There was a previously checked element
+                            // that has been permanently highlighted.
+                            // Reset the highlighting of the old
+                            // feature.
+                            this.resetHighlightFeature(this.currentPermanentHighlight)
+                        }
+                        if (e.target.id !== 'varWater') {
+                            // The element has a feature ID associated
+                            // with it.
+                            let featureID = e.target.value;
+                            this.highlightFeature(featureID);
+                            this.currentPermanentHighlight = featureID;
+                        } else {
+                            // The element is a variable water name item
+                            // and thus has no feature associated with
+                            // it.
+                            this.currentPermanentHighlight = null;
+                        }
+                    }
+                });
+            });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.registerChangeListener();
+    }
+
+    componentDidMount() {
+        this.registerChangeListener();
     }
 
     render(props, state, context) {
@@ -215,6 +401,7 @@ class WaterList extends Component {
             </div>`;
     }
 }
+
 
 render(
     html`
