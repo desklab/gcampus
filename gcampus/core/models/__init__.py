b048793851903f3def907696c3f988d861102fb8
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-05-30 18:45:18 +0200

    Fix imports

--- gcampus/core/models/__init__.py
+++ gcampus/core/models/__init__.py
@@ -1,2 +1,9 @@
-from gcampus.core.models.measurement import *
-from gcampus.core.models.token import *
+from gcampus.core.models.measurement import (
+    Measurement,
+    DataType,
+    DataPoint,
+)
+from gcampus.core.models.token import (
+    StudentToken,
+    TeacherToken,
+)
