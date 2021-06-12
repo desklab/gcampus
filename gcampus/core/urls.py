ef991e679e7968e90f1da63bc13099f5ef551707
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-06-12 14:32:58 +0200

    No breaks and change url for mapview (now root)

--- gcampus/core/urls.py
+++ gcampus/core/urls.py
@@ -1,43 +1,39 @@
 from django.urls import path
 
+from gcampus.core.views.datapoint import DataPointFormSetView
 from gcampus.core.views.measurement import (
     MeasurementFormView,
-    DataPointFormSetView,
     MeasurementListView,
     MeasurementDetailView,
     MeasurementMapView,
 )
-from gcampus.core.views.token import SetStudentTokenFormView, SetTeacherTokenFormView, logout
+from gcampus.core.views.token import (
+    SetStudentTokenFormView,
+    SetTeacherTokenFormView,
+    logout,
+)
 
+# Turn off black formatting and pylint
+# fmt: off
+# pylint: disable=line-too-long
 urlpatterns = [
+    path("", MeasurementMapView.as_view(), name="mapview"),
+    # Add measurement and data
     path("add/", MeasurementFormView.as_view(), name="add_measurement"),
-    path(
-        "add/<int:measurement_id>/data/",
-        DataPointFormSetView.as_view(),
-        name="add_data_points",
-    ),
+    path("add/<int:measurement_id>/data/", DataPointFormSetView.as_view(), name="add_data_points"),
+    # Edit measurement and data
+    # TODO @jnsdrtlf: editable measurement form
+    # path("edit/<int:measurement_id>", MeasurementFormView.as_view(), name="edit_measurement"),
+    path("edit/<int:measurement_id>/data/", DataPointFormSetView.as_view(), name="edit_data_points"),
+    # Measurement list and details
     path("measurements/", MeasurementListView.as_view(), name="measurements"),
-    path(
-        "measurement/<int:pk>/detail",
-        MeasurementDetailView.as_view(),
-        name="measurement_detail",
-    ),
-    path(
-        "mapview/",
-        MeasurementMapView.as_view(),
-        name="measurement_mapview",
-    ),
-    path(
-        "verify/student/",
-        SetStudentTokenFormView.as_view(),
-        name="student_token_form",
-    ),
-    path(
-        "verify/teacher/",
-        SetTeacherTokenFormView.as_view(),
-        name="teacher_token_form",
-    ),
+    path("measurement/<int:pk>/detail", MeasurementDetailView.as_view(), name="measurement_detail"),
+    # Token
+    path("verify/student/", SetStudentTokenFormView.as_view(), name="student_token_form"),
+    path("verify/teacher/", SetTeacherTokenFormView.as_view(), name="teacher_token_form"),
     path('logout/', logout),
 ]
+# fmt: on
+# pylint: enable=line-too-long
 
 app_name = "gcampuscore"
