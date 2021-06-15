9c8e29964e7d8d44bd2caee128663275ff662d63
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-06-15 11:23:17 +0200

    Fix inability to logout

--- gcampus/auth/utils.py
+++ gcampus/auth/utils.py
@@ -3,26 +3,30 @@ from typing import Optional
 from django.http import HttpRequest
 
 
+TOKEN_STORE = "gcampusauth_token"
+AUTHENTICATION_BOOLEAN = "gcampusauth_authenticated"
+
+
 def get_token(request: HttpRequest, default: str = None) -> Optional[str]:
-    if "gcampusauth_token" in request.session:
-        return request.session["gcampusauth_token"].get("token", default)
+    if TOKEN_STORE in request.session:
+        return request.session[TOKEN_STORE].get("token", default)
     return default
 
 
 def get_token_type(request: HttpRequest, default: str = None) -> Optional[str]:
-    if "gcampusauth_token" in request.session:
-        return request.session["gcampusauth_token"].get("token_type", default)
+    if TOKEN_STORE in request.session:
+        return request.session[TOKEN_STORE].get("token_type", default)
     return default
 
 
 def set_token(request: HttpRequest, token: str, token_type: str):
-    request.session["gcampusauth_token"] = {
+    request.session[TOKEN_STORE] = {
         "token": token,
         "token_type": token_type
     }
-    request.session["gcampusauth_authenticated"] = True
+    request.session[AUTHENTICATION_BOOLEAN] = True
 
 
 def logout(request: HttpRequest):
-    request.session["gcampusauth_token"] = {}
-    request.session["gcampusauth_authorized"] = False
+    request.session[TOKEN_STORE] = {}
+    request.session[AUTHENTICATION_BOOLEAN] = False
