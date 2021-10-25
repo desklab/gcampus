6580f70f0ed4d9b14ea4bbd8d4211786c3fb81fc
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-10-25 23:28:45 +0200

    Specify default auto field

--- gcampus/settings/base.py
+++ gcampus/settings/base.py
@@ -98,6 +98,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
