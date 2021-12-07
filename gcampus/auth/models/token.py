813c16ed18cbc060e1d78b456d37d24e561d0625
Jonas Drotleff <j.drotleff@desk-lab.de>
2021-12-07 17:14:54 +0100

    Fix deprecation of ugettext

--- gcampus/auth/models/token.py
+++ gcampus/auth/models/token.py
@@ -19,7 +19,7 @@ from typing import Union, Optional, Tuple
 from django.conf import settings
 from django.contrib.gis.db import models
 from django.utils.crypto import get_random_string
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from gcampus.core.models import Measurement
 from gcampus.core.models.util import DateModelMixin
