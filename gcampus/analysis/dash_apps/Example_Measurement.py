a9da76813a6f2308485200bc788dce4967e95275
nlwach <56191503+nlwach@users.noreply.github.com>
2021-12-14 14:33:26 +0100

    clean up plot code a little

--- gcampus/analysis/dash_apps/Example_Measurement.py
+++ gcampus/analysis/dash_apps/Example_Measurement.py
@@ -1,71 +1,65 @@
-from gcampus.settings.analysis import LIMITS, PLOT_TYPES, SCATTER_PLOT, HISTOGRAM, BOXPLOT
+from gcampus.settings.analysis import PLOT_TYPES, SCATTER_PLOT, HISTOGRAM, BOXPLOT
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 from gcampus.analysis.util import database_query_for_plot, remove_one_day, add_one_day
 
-from datetime import date
 
 import pandas as pd
 from django_plotly_dash import DjangoDash
 
-from gcampus.core.models import ParameterType, Measurement
+from gcampus.core.models import ParameterType, Limit
 
 app = DjangoDash("ExampleMeasurement")
 
-parameter_types_names = [i.name for i in list(ParameterType.objects.all())]
-parameter_types = list(ParameterType.objects.all())
 
-water_names = list(set(Measurement.objects.values_list('water_name', flat=True).distinct()))
-
-months = pd.date_range('2020-1-10', date.today().strftime("%Y-%m-%d"),
-              freq='MS').strftime("%Y-%m").tolist()
-
-app.layout = html.Div([
-    html.Div([
+def create_graph(app, parameter_types_names, parameter_types, water_names, months):
+    app.layout = html.Div([
         html.Div([
-            dcc.Dropdown(
-                id='parameter_type',
-                options=[{'label': i, 'value': i} for i, j in zip(parameter_types_names, parameter_types)],
-                # if statement needed for tests, error if parameter_types is empty
-                value=parameter_types[0].name if parameter_types else None
-            ),
-            dcc.RadioItems(
-                id='plot-type',
-                options=[{'label': i, 'value': i} for i in PLOT_TYPES],
-                value='Scatter',
-                labelStyle={'display': 'inline-block'}
-            ),
-            dcc.RadioItems(
-                id='limits',
-                options=[{'label': i, 'value': j} for i, j in zip(["Show Limits", "Hide Limits"], [1, 0])],
-                value=1,
-                labelStyle={'display': 'inline-block'}
-            )
-        ], style={'width': '48%', 'display': 'inline-block'}),
+            html.Div([
+                dcc.Dropdown(
+                    id='parameter_type',
+                    options=[{'label': i, 'value': i} for i, j in zip(parameter_types_names, parameter_types)],
+                    # if statement needed for tests, error if parameter_types is empty
+                    value=parameter_types[0].name if parameter_types else None
+                ),
+                dcc.RadioItems(
+                    id='plot-type',
+                    options=[{'label': i, 'value': i} for i in PLOT_TYPES],
+                    value='Scatter',
+                    labelStyle={'display': 'inline-block'}
+                ),
+                dcc.RadioItems(
+                    id='limits',
+                    options=[{'label': i, 'value': j} for i, j in zip(["Show Limits", "Hide Limits"], [1, 0])],
+                    value=1,
+                    labelStyle={'display': 'inline-block'}
+                )
+            ], style={'width': '48%', 'display': 'inline-block'}),
 
-        html.Div([
-            dcc.Dropdown(
-                id='water_name',
-                options=[{'label': i, 'value': i} for i in water_names],
-                # if statement needed for tests, error if water_names is empty
-                value=water_names[0] if water_names else None
-            ),
+            html.Div([
+                dcc.Dropdown(
+                    id='water_name',
+                    options=[{'label': i, 'value': i} for i in water_names],
+                    # if statement needed for tests, error if water_names is empty
+                    value=water_names[0] if water_names else None
+                ),
 
-        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
+            ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
 
-    ]),
-    dcc.Graph(id='indicator-graphic'),
-    dcc.Slider(
-        id='year--slider',
-        min=months[0],
-        max=months[-1],
-        value=months[0],
-        marks={str(month): str(month) for month in months},
-        step=None
-    ),
-])
+        ]),
+        dcc.Graph(id='indicator-graphic'),
+        dcc.Slider(
+            id='year--slider',
+            min=months[0],
+            max=months[-1],
+            value=months[0],
+            marks={str(month): str(month) for month in months},
+            step=None
+        ),
+    ])
+    return app
 
 
 @app.callback(
@@ -78,6 +72,7 @@ app.layout = html.Div([
 def update_graph(parameter_type, water_name, plot_type, show_limit, slider):
     if not isinstance(parameter_type, ParameterType):
         parameter_type = ParameterType.objects.get(name=parameter_type)
+    limits = Limit.objects.filter(parameter_limit=parameter_type)
 
     x, y = database_query_for_plot(water_name, parameter_type, 0)
 
@@ -94,29 +89,17 @@ def update_graph(parameter_type, water_name, plot_type, show_limit, slider):
 
     if plot_type == SCATTER_PLOT:
         fig = px.scatter(df, x="Time", y="Values")
-        if parameter_type.id in LIMITS.keys() and show_limit:
-            limit = LIMITS[parameter_type.id]
+        if limits and show_limit:
 
             max_time = max(df["Time"])
             max_time = add_one_day(max_time, "%Y-%m-%d")
 
             min_time = min(df["Time"])
             min_time = remove_one_day(min_time, "%Y-%m-%d")
+            for limit in limits:
+                fig.add_hline(y=limit.limit_value, line_color=limit.graph_color, line_dash="dash", name=limit.limit_type)
 
-            fig.add_shape(type='line',
-                          x0=min_time,
-                          y0=limit,
-                          x1=max_time,
-                          y1=limit,
-                          line=dict(color='Red', ),
-                          xref='x',
-                          yref='y'
-                          )
-            fig.update_layout(margin={'l': 0, 'b': 40, 't': 10, 'r': 0}, hovermode='closest',
-                              yaxis_range=[min(min(df["Values"]), limit) - 5, max(max(df["Values"]), limit) + 5],
-                              xaxis_range=[min_time, max_time])
-        else:
-            fig.update_layout(margin={'l': 0, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+        fig.update_layout(margin={'l': 0, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
 
     elif plot_type == HISTOGRAM:
         fig = px.histogram(df, x="Values")
--- gcampus/analysis/views.py
+++ gcampus/analysis/views.py
@@ -1,9 +1,24 @@
+from datetime import date
+
+import pandas as pd
 from django.shortcuts import render
+from gcampus.analysis.dash_apps.Example_Measurement import create_graph, app
+from gcampus.core.models import ParameterType, Measurement
 
 
 def analysis(requests):
     return render(requests, "gcampusanalysis/analysis.html")
 
 def analysis_measurement(requests):
+    parameter_types_names = [i.name for i in list(ParameterType.objects.all())]
+    parameter_types = list(ParameterType.objects.all())
+
+    water_names = list(set(Measurement.objects.values_list('water_name', flat=True).distinct()))
+
+    months = pd.date_range('2020-1-10', date.today().strftime("%Y-%m-%d"),
+                           freq='MS').strftime("%Y-%m").tolist()
+
+    create_graph(app, parameter_types_names, parameter_types, water_names, months)
+
     return render(requests, "gcampusanalysis/analysis_measurement.html")
 
