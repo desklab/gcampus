a9f289beab1fc1a7d5afaf10db7310987c82daf0
Jonas Drotleff <j.drotleff@desk-lab.de>
2022-04-20 09:22:09 +0200

    Revert "Reduce worker to 1"
    This reverts commit 0054f8e9d91128383a0efbc4750168967b6bab35.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -18,5 +18,5 @@ errorlog = "-"  # log to stderr
 loglevel = "info"
 # capture_output = True
 bind = "0.0.0.0:8000"
-workers = 1
+workers = 4
 worker_class = "sync"  # default worker. Change if needed
