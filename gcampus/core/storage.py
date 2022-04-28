828a0a71d92ba5baa3a69011d5fcb68dc3736767
Jonas Drotleff <j.drotleff@desk-lab.de>
2022-04-28 22:19:34 +0200

    Use S3 storage for documents

--- gcampus/core/storage.py
+++ gcampus/core/storage.py
@@ -13,49 +13,28 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import functools
-
-from django.conf import settings
 from django.utils.deconstruct import deconstructible
-from storages.backends.s3boto3 import S3StaticStorage  # noqa
+from storages.backends.s3boto3 import S3StaticStorage, S3Boto3Storage
 
 
 @deconstructible
-class StaticFileStorage(S3StaticStorage):  # noqa
-    """Static File Storage Class"""
+class MediaStorage(S3Boto3Storage):
+    location = "media"
 
-    # The location basically equates to the bucket
-    location = getattr(settings, "STATIC_LOCATION", "static")
 
-    # Set the default permission for the static files bucket. Should be
-    # publicly readable.
-    default_acl = "public-read"
+@deconstructible
+class StaticStorage(S3StaticStorage):
+    location = "static"
 
-    # Disable authentication using query strings. This is done to avoid
-    # generating a custom access token for every request, which would be
-    # a waste of time.
-    # Static resources should be publicly accessible.
-    querystring_auth = False
-
-    # Allow files to be overwritten. This is very useful for static
-    # files, as we do not want to have a million different versions
-    # of a single file.
-    file_overwrite = True
-
-    @property
-    @functools.lru_cache
-    def _static_url(self):
-        return "{endpoint:s}/{bucket:s}/".format(
-            endpoint=self.endpoint_url,
-            bucket=self.bucket_name,
+    def get_default_settings(self) -> dict:
+        settings = super(StaticStorage, self).get_default_settings()
+        protocol, host = settings["endpoint_url"].split("//")
+        bucket_name = settings["bucket_name"]
+        settings.update(
+            {
+                "custom_domain": f"{host}/{bucket_name}",
+                "url_protocol": protocol,
+                "secure_urls": bool(protocol == "https:"),
+            }
         )
-
-    def url(self, name, parameters=None, expire=None, http_method=None):
-        if self.querystring_auth:
-            super(StaticFileStorage, self).url(
-                name, parameters=parameters, expire=expire, http_method=http_method
-            )
-        else:
-            # ``_static_url`` has a tailing slash while
-            # ``_normalize_name`` has no leading slash.
-            return self._static_url + self._normalize_name(self._clean_name(name))
+        return settings
