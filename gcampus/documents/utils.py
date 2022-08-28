56299888e77ef58dfbe1fc19ab2b515f976ee177
Jonas Drotleff <j.drotleff@desk-lab.de>
2022-08-28 15:30:46 +0200

    Fix: Fetch doc static files from S3; Bump version

--- gcampus/documents/utils.py
+++ gcampus/documents/utils.py
@@ -16,7 +16,9 @@
 import mimetypes
 import os
 import posixpath
+import urllib
 from pathlib import Path
+from urllib.parse import urlsplit
 
 from django.conf import settings
 from django.contrib.sessions.middleware import SessionMiddleware
@@ -24,8 +26,9 @@ from django.contrib.staticfiles import finders
 from django.http import HttpRequest
 from weasyprint.urls import URLFetchingError, default_url_fetcher
 
+from gcampus.core import get_base_url
+
 STATIC_FILES_PATH = Path(__file__).resolve().parent / "static"
-URI_IDENTIFIER = "gcampusdocuments"
 
 
 def _mock_get_response():
@@ -42,24 +45,31 @@ def mock_request() -> HttpRequest:
 
 
 def url_fetcher(url: str, **kwargs):
-    if url.startswith(f"{URI_IDENTIFIER}:"):
-        # Remove the URI identifier only used to trigger the URL fetcher
-        url = url[len(URI_IDENTIFIER) + 1 :]
-    if url.startswith(settings.STATIC_URL):
-        path = url.replace(settings.STATIC_URL, "", 1)
+    try:
+        o = urlsplit(url)
+    except ValueError:
+        return default_url_fetcher(url, **kwargs)
+    if o.netloc == settings.PRIMARY_HOST and o.path.startswith(settings.STATIC_URL):
+        path = os.path.relpath(o.path, settings.STATIC_URL)
         normalized_path = posixpath.normpath(path).lstrip("/")
-        absolute_path = finders.find(normalized_path)
-        if not absolute_path:
-            raise FileNotFoundError()
-        if not os.path.isfile(absolute_path):
-            raise URLFetchingError(
-                f"File '{url}' (resolved to '{absolute_path}') not found!"
-            )
-        mime_type, encoding = mimetypes.guess_type(absolute_path, strict=True)
+        mime_type, encoding = mimetypes.guess_type(path, strict=True)
+        if getattr(settings, "AWS_STATIC_USE_STATIC_URL", False):
+            from django.contrib.staticfiles.storage import staticfiles_storage
+
+            file_obj = staticfiles_storage.open(normalized_path)
+        else:
+            absolute_path = finders.find(normalized_path)
+            if not absolute_path:
+                raise FileNotFoundError()
+            if not os.path.isfile(absolute_path):
+                raise URLFetchingError(
+                    f"File '{url}' (resolved to '{absolute_path}') not found!"
+                )
+            file_obj = open(absolute_path, "rb")
         return {
             "mime_type": mime_type,
             "encoding": encoding,
-            "file_obj": open(absolute_path, "rb"),
+            "file_obj": file_obj,
         }
     else:
         return default_url_fetcher(url, **kwargs)
