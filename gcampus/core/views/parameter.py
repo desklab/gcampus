d9ac7af9e419cc292dfa72519f1f5444c0b6cfd9
Axel Schlindwein <a.schlindwein@desk-lab.de>
2022-11-10 12:04:40 +0100

    use common base class for parameter form set view

--- gcampus/core/views/parameter.py
+++ gcampus/core/views/parameter.py
@@ -33,13 +33,79 @@ from gcampus.core.forms.measurement import (
     ChemicalParameterFormSet,
     BiologicalParameterFormSet,
     StructureIndexForm,
+    BaseParameterFormset,
 )
 from gcampus.core.models import Parameter, Measurement
 from gcampus.core.models.index.structure import StructureIndex
 from gcampus.core.views.base import TitleMixin
 
 
-class ChemicalParameterFormSetView(TitleMixin, TemplateResponseMixin, View):
+class BaseParameterFormSetView(TitleMixin, TemplateResponseMixin, View):
+    formset_class = BaseParameterFormset
+
+    def get(self, request: HttpRequest, pk: int, *args, **kwargs):
+        # This will raise an exception if the provided token has
+        # insufficient permissions
+        formset = self.get_formset(request, pk)
+        return self.render_to_response(self.get_context_data(formset=formset))
+
+    def get_context_data(self, **kwargs):
+        if "object" not in kwargs:
+            kwargs["object"] = self.instance
+        return super().get_context_data(**kwargs)
+
+    def get_formset(self, request: HttpRequest, pk: int) -> formset_class:
+        """Get Formset
+
+        Get the formset for the current request based on the instance of
+        the specified measurement ID.
+        Will raise a 404 error if no measurement with such ID exists.
+        This method will also check whether the provided token (taken
+        from the session), is valid and allowed to edit a measurement.
+        Otherwise, a :class:`django.core.exceptions.PermissionDenied`
+        exception is raised.
+
+        :param request: The request object. Contains e.g. the POST form
+            data.
+        :param pk: This information comes from the URL and
+            contains the ID to the measurement entry that is being
+            edited.
+        """
+        self.instance = get_object_or_404(Measurement, id=pk)
+        self.token: BaseToken = request.token
+        if self.token is None:
+            raise UnauthenticatedError()
+        if not self.token.has_perm("gcampuscore.change_measurement", obj=self.instance):
+            raise TokenEditPermissionError()
+        parameter_perms = [
+            "gcampuscore.add_parameter",
+            "gcampuscore.change_parameter",
+            "gcampuscore.delete_parameter",
+        ]
+        if not self.token.has_perms(parameter_perms):
+            raise TokenEditPermissionError()
+        if request.method == "POST":
+            return self.formset_class(
+                data=request.POST, files=request.FILES, instance=self.instance
+            )
+        else:
+            return self.formset_class(instance=self.instance)
+
+    def form_valid(self, formset: formset_class):
+        formset.save()
+        return HttpResponseRedirect(self.get_next_url())
+
+    def post(self, request: HttpRequest, pk: int, *args, **kwargs):
+        # This will raise an exception if the provided token has
+        # insufficient permissions
+        formset = self.get_formset(request, pk)
+        if formset.is_valid():
+            return self.form_valid(formset)
+        else:
+            return self.render_to_response(self.get_context_data(formset=formset))
+
+
+class ChemicalParameterFormSetView(BaseParameterFormSetView):
     formset_class = ChemicalParameterFormSet
     template_name = "gcampuscore/forms/parameters-chemical.html"
 
@@ -57,76 +123,13 @@ class ChemicalParameterFormSetView(TitleMixin, TemplateResponseMixin, View):
             next_view_name = "gcampuscore:measurement-detail"
         return reverse(next_view_name, kwargs={"pk": self.instance.pk})
 
-    def get_context_data(self, **kwargs):
-        if "object" not in kwargs:
-            kwargs["object"] = self.instance
-        return super().get_context_data(**kwargs)
-
     def __init__(self, **kwargs):
         super(ChemicalParameterFormSetView, self).__init__(**kwargs)
         self.instance: Optional[Parameter] = None
         self.token: Optional[BaseToken] = None
 
-    def form_valid(self, formset: ChemicalParameterFormSet):
-        formset.save()
-        return HttpResponseRedirect(self.get_next_url())
 
-    def get_formset(
-        self, request: HttpRequest, pk: int
-    ) -> ChemicalParameterFormSetView.formset_class:
-        """Get Formset
-
-        Get the formset for the current request based on the instance of
-        the specified measurement ID.
-        Will raise a 404 error if no measurement with such ID exists.
-        This method will also check whether the provided token (taken
-        from the session), is valid and allowed to edit a measurement.
-        Otherwise, a :class:`django.core.exceptions.PermissionDenied`
-        exception is raised.
-
-        :param request: The request object. Contains e.g. the POST form
-            data.
-        :param pk: This information comes from the URL and
-            contains the ID to the measurement entry that is being
-            edited.
-        """
-        self.instance = get_object_or_404(Measurement, id=pk)
-        self.token: BaseToken = request.token
-        if self.token is None:
-            raise UnauthenticatedError()
-        if not self.token.has_perm("gcampuscore.change_measurement", obj=self.instance):
-            raise TokenEditPermissionError()
-        parameter_perms = [
-            "gcampuscore.add_parameter",
-            "gcampuscore.change_parameter",
-            "gcampuscore.delete_parameter",
-        ]
-        if not self.token.has_perms(parameter_perms):
-            raise TokenEditPermissionError()
-        if request.method == "POST":
-            return self.formset_class(
-                data=request.POST, files=request.FILES, instance=self.instance
-            )
-        else:
-            return self.formset_class(instance=self.instance)
-
-    def post(self, request: HttpRequest, pk: int, *args, **kwargs):
-        # This will raise an exception if the provided token has
-        # insufficient permissions
-        formset = self.get_formset(request, pk)
-        if formset.is_valid():
-            return self.form_valid(formset)
-        else:
-            return self.render_to_response(self.get_context_data(formset=formset))
-
-    def get(self, request: HttpRequest, pk: int, *args, **kwargs):
-        # This will raise an exception if the provided token has
-        # insufficient permissions
-        formset = self.get_formset(request, pk)
-        return self.render_to_response(self.get_context_data(formset=formset))
-
-
-class BiologicalParameterFormSetView(TitleMixin, TemplateResponseMixin, View):
+class BiologicalParameterFormSetView(BaseParameterFormSetView):
     formset_class = BiologicalParameterFormSet
     template_name = "gcampuscore/forms/parameters-biological.html"
     next_view_name = "gcampuscore:edit-structure-index"
@@ -139,73 +142,10 @@ class BiologicalParameterFormSetView(TitleMixin, TemplateResponseMixin, View):
     def get_next_url(self):
         return reverse(self.next_view_name, kwargs={"pk": self.instance.pk})
 
-    def get_context_data(self, **kwargs):
-        if "object" not in kwargs:
-            kwargs["object"] = self.instance
-        return super().get_context_data(**kwargs)
-
     def __init__(self, **kwargs):
         super(BiologicalParameterFormSetView, self).__init__(**kwargs)
         self.instance: Optional[Parameter] = None
         self.token: Optional[BaseToken] = None
-
-    def form_valid(self, formset: BiologicalParameterFormSet):
-        formset.save()
-        return HttpResponseRedirect(self.get_next_url())
-
-    def get_formset(
-        self, request: HttpRequest, pk: int
-    ) -> BiologicalParameterFormSetView.formset_class:
-        """Get Formset
-
-        Get the formset for the current request based on the instance of
-        the specified measurement ID.
-        Will raise a 404 error if no measurement with such ID exists.
-        This method will also check whether the provided token (taken
-        from the session), is valid and allowed to edit a measurement.
-        Otherwise, a :class:`django.core.exceptions.PermissionDenied`
-        exception is raised.
-
-        :param request: The request object. Contains e.g. the POST form
-            data.
-        :param pk: This information comes from the URL and
-            contains the ID to the measurement entry that is being
-            edited.
-        """
-        self.instance = get_object_or_404(Measurement, id=pk)
-        self.token: BaseToken = request.token
-        if self.token is None:
-            raise UnauthenticatedError()
-        if not self.token.has_perm("gcampuscore.change_measurement", obj=self.instance):
-            raise TokenEditPermissionError()
-        parameter_perms = [
-            "gcampuscore.add_parameter",
-            "gcampuscore.change_parameter",
-            "gcampuscore.delete_parameter",
-        ]
-        if not self.token.has_perms(parameter_perms):
-            raise TokenEditPermissionError()
-        if request.method == "POST":
-            return self.formset_class(
-                data=request.POST, files=request.FILES, instance=self.instance
-            )
-        else:
-            return self.formset_class(instance=self.instance)
-
-    def post(self, request: HttpRequest, pk: int, *args, **kwargs):
-        # This will raise an exception if the provided token has
-        # insufficient permissions
-        formset = self.get_formset(request, pk)
-        if formset.is_valid():
-            return self.form_valid(formset)
-        else:
-            return self.render_to_response(self.get_context_data(formset=formset))
-
-    def get(self, request: HttpRequest, pk: int, *args, **kwargs):
-        # This will raise an exception if the provided token has
-        # insufficient permissions
-        formset = self.get_formset(request, pk)
-        return self.render_to_response(self.get_context_data(formset=formset))
 
 
 class StructureIndexFormViewMixin(TitleMixin, ModelFormMixin):
