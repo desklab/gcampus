08150f1aa6a1ad962733ce631cd8926bb87a55f0
Jonas Drotleff <j.drotleff@desk-lab.de>
2022-11-28 16:32:04 +0100

    Add environment variable for overpass server

--- gcampus/settings/base.py
+++ gcampus/settings/base.py
@@ -200,8 +200,10 @@ MEASUREMENT_MAX_TIME = datetime.datetime(2100, month=1, day=1, tzinfo=utc)
 NOMINATIM_DOMAIN = "nominatim.openstreetmap.org"
 GEOLOCKUP_CACHE_TIMEOUT = 60 * 60 * 24 * 100  # 100 days
 
-# OVERPASS_SERVER = "https://overpass-api.de/api/interpreter"
-OVERPASS_SERVER = "https://overpass.kumi.systems/api/interpreter"
+# Alternative OSM overpass server: "https://overpass-api.de/api/interpreter"
+OVERPASS_SERVER = get_env_read_file(
+    "GCAMPUS_OVERPASS_SERVER", "https://overpass.kumi.systems/api/interpreter"
+)
 OVERPASS_CACHE = 60 * 60 * 24 * 2
 OVERPASS_TIMEOUT = 20  # Timeout in seconds
 REQUEST_TIMEOUT = 5  # Short timeout for simple requests
