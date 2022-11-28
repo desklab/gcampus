c9478762c22b92cb2ec1aa379ef6ce750f5ef6af
Axel Schlindwein <a.schlindwein@desk-lab.de>
2022-11-28 16:52:50 +0100

    add data quality flag and parameter limits (#83)
    * add data quality flag by checking parameter limits

--- gcampus/core/receivers.py
+++ gcampus/core/receivers.py
@@ -117,6 +117,20 @@ def update_measurement_indices(sender, instance: Parameter, **kwargs):
         instance.measurement.structure_index.update()
 
 
+@receiver(post_save, sender=Parameter)
+def check_warning_limits(sender, instance: Parameter, **kwargs):
+    if instance.parameter_type.lower_warning_limit is not None:
+        if instance.value < instance.parameter_type.lower_warning_limit:
+            instance.measurement.requires_review = True
+            instance.measurement.save()
+            return
+    if instance.parameter_type.upper_warning_limit is not None:
+        if instance.value > instance.parameter_type.upper_warning_limit:
+            instance.measurement.requires_review = True
+            instance.measurement.save()
+            return
+
+
 @receiver(post_save, sender=Measurement)
 def create_measurement_indices(
     sender, instance: Measurement, created: bool = False, **kwargs
