2f3bfdf52a17636b42d7a2f792c3355638d23b96
Jonas Drotleff <j.drotleff@desk-lab.de>
2023-06-03 20:07:47 +0300

    Docker: Cleanup Dockerfile and bump Python version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,7 +20,7 @@ set -eo pipefail
 shopt -s nullglob
 
 # Migrate database
-/srv/gcampus/venv/bin/python3 manage.py migrate
+python3 manage.py migrate
 
 # Launch gunicorn server
-/srv/gcampus/venv/bin/gunicorn
+gunicorn
