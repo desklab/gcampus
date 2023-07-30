467a44187ae9722529535b057c2231800c2e520d
Jonas Drotleff <j.drotleff@desk-lab.de>
2023-07-30 18:48:57 +0300

    Code quality: Remove old 'LocationRadiusField'

--- gcampus/core/fields/__init__.py
+++ gcampus/core/fields/__init__.py
@@ -13,7 +13,6 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ["SplitSplitDateTimeField", "LocationRadiusField"]
+__all__ = ["SplitSplitDateTimeField"]
 
 from gcampus.core.fields.datetime import SplitSplitDateTimeField
-from gcampus.core.fields.geo import LocationRadiusField
